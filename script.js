26d0a2c5580b890545b8333c24f8be2ce46a00ce
faidfadjri <faidfadjri.busdev@gmail.com>
2023-12-29 22:23:35 +0700

    fix : scroll on target

---
+++ script.js
@@ -0,0 +1,17 @@
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute("href");
+    const targetElement = document.querySelector(targetId);
+
+    if (targetElement) {
+      const offsetTop = targetElement.offsetTop - 80; // Adjust the offset as needed
+
+      window.scrollTo({
+        top: offsetTop,
+        behavior: "smooth",
+      });
+    }
+  });
+});
